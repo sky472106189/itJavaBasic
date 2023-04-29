612cd664f118e1eae71cce5e79c875cb4ca8768f
sky472106189 <ciweiyu1996163.com>
2023-04-29 10:00:24 +0800

    方法重载

--- src/com/itpw/day01/method/MethodDemo03.java
+++ src/com/itpw/day01/method/MethodDemo03.java
@@ -2,13 +2,15 @@ package com.itpw.day01.method;
 
 /*
 * 方法重载
-*   同一个类名下,方法名相同,形参不同(个数，类型,顺序)，更易于以后的代码编写
+*   同一个类名下,方法名相同,形参不同(个数，类型,顺序)，返回值可以相同，更易于以后的代码编写
 * */
 public class MethodDemo03 {
     public static void main(String[] args) {
         int x = sum(1,2);
         long y = sum(1L,2L);
         double z = sum(1.0,2.0);
+        // 返回值相同的重载
+        int x1 = sum(1,2L);
     }
 
     public static int sum(int a,int b){
@@ -20,4 +22,5 @@ public class MethodDemo03 {
     public static double sum(double a,double b){
         return a+b;
     }
+    public static int sum(int a,long b){return a+(int)b;}
 }
