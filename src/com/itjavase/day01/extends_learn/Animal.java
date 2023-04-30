e311a422a9e9ed744b620f318803c3e0214c8726
sky472106189 <ciweiyu1996163.com>
2023-04-30 16:41:14 +0800

    继承

---
+++ src/com/itjavase/day01/extends_learn/Animal.java
@@ -0,0 +1,20 @@
+package com.itjavase.day01.extends_learn;
+
+public class Animal {
+    private String name;
+
+    public void move(){
+        // 为什么对私有对象直接name是可以的？
+        // 这是因为private是允许在本类中直接调用的
+        System.out.println(name+"在移动");
+        System.out.println(getName()+"在移动");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/com/itjavase/day01/extends_learn/Cat.java
@@ -0,0 +1,7 @@
+package com.itjavase.day01.extends_learn;
+
+public class Cat extends Animal{
+
+    public String name;
+
+}
