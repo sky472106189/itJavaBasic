253b8043778d075a673e657e1fbea6d4d38a26c4
sky472106189 <ciweiyu1996163.com>
2023-04-30 16:41:25 +0800

    构造方法执行并不一定会创建对象

---
+++ src/com/itjavase/demo/Demo06.java
@@ -0,0 +1,28 @@
+package com.itjavase.demo;
+
+/*
+* 构造方法执行并不一定会创建对象
+* 摘自:https://www.cnblogs.com/zhangjianbing/p/8072821.html
+* */
+public class Demo06 {
+    public static void main(String[] args) {
+        // 抽象类无法new对象
+//        AbstractObject obj = new AbstractObject();
+        SomeObject obj = new SomeObject();
+    }
+}
+
+abstract class AbstractObject {
+
+    public AbstractObject() {
+        System.out.println("抽象类的构造方法执行了!");
+    }
+
+}
+class SomeObject extends AbstractObject {
+
+    public SomeObject() {
+        System.out.println("子类的构造方法执行了!");
+    }
+
+}
