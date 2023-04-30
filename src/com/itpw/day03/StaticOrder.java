f43b4727cfda1d3a587a66bf35e6e307e5c6738e
sky472106189 <ciweiyu1996163.com>
2023-04-30 14:04:57 +0800

    注释优化

--- src/com/itpw/day03/StaticOrder.java
+++ src/com/itpw/day03/StaticOrder.java
@@ -11,17 +11,25 @@ public class StaticOrder {
     // 实例变量 new对象才会存在
     int k = 111;
 
-
     // 静态代码块 类加载时执行
     static {
         // 能访问静态变量i 不能访问实例变量k
+        System.out.println(i);
         // 这样才能访问k
-//        StaticOrder obj = new StaticOrder();
-//        System.out.println(obj.k);
+        StaticOrder obj = new StaticOrder();
+        System.out.println(obj.k);
 
         // 无法访问静态变量name 因为静态是自上而下的顺序
-        //System.out.println(name);
+        // 注意静态方法不需要顺序，比如可以直接调用main方法
+        // 但是静态变量是有顺序的 可以参考com.itjavaee.day01.staticdemo.Demo03
+//        System.out.println(name);
+//        System.out.println(j);
+        main(new String[]{"1","2"});
     }
+
+    int m;
+
+    static int j;
 
     static String name = "ABC";
 
