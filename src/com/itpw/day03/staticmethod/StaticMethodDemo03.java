25fa31026c087a5601ec077dbf8a9e680a397ecb
sky472106189 <ciweiyu1996163.com>
2023-05-01 10:15:21 +0800

    静态方法和实例方法的一些规范问题

---
+++ src/com/itpw/day03/staticmethod/StaticMethodDemo03.java
@@ -0,0 +1,50 @@
+package com.itpw.day03.staticmethod;
+
+/*
+* 同一个类中静态和实例的调用问题
+*   1、实例方法能调用静态方法和静态变量。
+*   2、静态方法只能调用静态方法和静态变量，无法调用实例方法和实例变量
+* */
+public class StaticMethodDemo03 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String staticName;
+    public String name;
+
+    public void func1(){
+        // this.可以省略，但是最好别省
+        System.out.println(this.name);// 不推荐
+        System.out.println(name);// 推荐
+        // this.staticName 可行，但是不推荐，对所有的静态变量都不应该使用this.
+        // 这和静态方法的调用要使用【类名.】是一个道理。
+        System.out.println(this.staticName);    // 不推荐
+        System.out.println(StaticMethodDemo03.staticName);  // 推荐
+
+        // 同一个类中的实例方法，前面的this.可以省略，但是最好别省
+        // 为什么？为了增强代码的可读性，让程序员知道func2()是一个实例方法
+        this.func2();
+
+        // 同一个类中的静态方法，可以省略前面的【类名.】,但是最好别省
+        // 不省是为了增强代码的可读性
+        StaticMethodDemo03.staticFunc1(); //推荐
+        staticFunc2(); // 不推荐
+
+        System.out.println("func1");
+    }
+
+    public void func2(){
+        System.out.println("func2");
+    }
+
+    public static void staticFunc1(){
+        System.out.println("staticFunc1");
+    }
+
+    public static void staticFunc2(){
+        System.out.println("staticFunc2");
+    }
+
+}
