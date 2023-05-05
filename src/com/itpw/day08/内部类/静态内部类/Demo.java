5e37f7f565733a1d34479f6f6864c586fc165926
sky472106189 <ciweiyu1996163.com>
2023-05-05 17:16:55 +0800

    内部类注释优化

--- src/com/itpw/day08/内部类/静态内部类/Demo.java
+++ src/com/itpw/day08/内部类/静态内部类/Demo.java
@@ -7,7 +7,7 @@ public class Demo {
     public static void main(String[] args) {
         // 静态内部类的定义方式，注意和实例内部类定义的区别
         // 1、静态内部类new Outer.Inner()
-        // 2、实例内部类new Car().new Engine(); 多一个括号
+        // 2、实例内部类new Car().new Engine(); 多一个括号多一个new
         Outer.Inner inner = new Outer.Inner();
         inner.test();
     }
